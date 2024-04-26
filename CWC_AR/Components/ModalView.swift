e58e605c719a3194a36623cb88684d5001a1f3de
anggaraswn <anggara11swn@gmail.com>
2024-04-26 22:55:00 +0700

    "Combining Speech, Leviosa, Ridikulus into 1 View & trying to fix drag gesture in Leviosa spell"

--- CWC_AR/Components/ModalView.swift
+++ CWC_AR/Components/ModalView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct ModalView: View {
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @Binding var isRecording:Bool
+    @Binding var color:Color
+    
+    @Binding var speechText:String
+    @Binding var speechAction: SpeechAction
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            Text("\((isRecording == false) ? "":((speechRecognizer.transcript == "") ? "Speak Now": speechRecognizer.transcript))")
+                .frame(width: 300, height: 200, alignment: .center)
+            Spacer()
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+                .scaleEffect(2)
+            Spacer()
+        }
+        .onAppear{
+            speechRecognizer.transcribe()
+            color = .red
+        }
+        .onDisappear{
+            color = .gray
+            speechRecognizer.stopTranscribing()
+            speechText = speechRecognizer.transcript
+            if speechText == "Reset" {
+                speechAction = .remove
+            } else if speechText == "Plane"{
+                speechAction = .plane
+            } else if speechText == "Drummer"{
+                speechAction = .drummer
+            }
+            else if speechText == "Ridiculous" || speechText == "Ridikulus" {
+                speechAction = .ridikulus
+            }
+        }
     }
 }
 
 #Preview {
-    ModalView()
+    ModalView(isRecording: .constant(false), color: .constant(.gray), speechText: .constant(""), speechAction: .constant(.none))
 }
